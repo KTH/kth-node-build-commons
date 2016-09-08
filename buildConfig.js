3dcde494b2482eacd723c797d805bc2c78f9491b
Simon Ingeson <simoni@kth.se>
2016-09-08 15:38:31 -0400

    allow setting custom config base path

--- buildConfig.js
+++ buildConfig.js
@@ -1,36 +1,38 @@
 'use strict'
 const configurator = require('kth-node-configuration')
+const path = require('path')
 let paths
 
 module.exports = {
   setPaths(_paths) {
     paths = _paths
   },
-  createFile() {
+  createFile(configBasePath) {
     /*
       This builds the file public/js/app/config.js with
       safe configuration content
     */
 
-    ['prod', 'ref', 'dev'].forEach(env => {
+    configBasePath = configBasePath || './config'
+
+    ;['prod', 'ref', 'dev'].forEach(env => {
       process.env.NODE_ENV = env
 
       const config = configurator({
-        defaults: require('config/commonSettings'),
-        local: require('config/localSettings'),
-        ref: require('config/refSettings'),
-        prod: require('config/prodSettings'),
-        dev: require('config/devSettings')
+        defaults: require(path.join(configBasePath, '/commonSettings')),
+        local: require(path.join(configBasePath, '/localSettings')),
+        ref: require(path.join(configBasePath, '/refSettings')),
+        prod: require(path.join(configBasePath, '/prodSettings')),
+        dev: require(path.join(configBasePath, '/devSettings'))
       })
 
       const safeConf = config.safe()
       const fs = require('fs')
       const stream = fs.createWriteStream(`public/js/app/config-${env}.js`)
 
-      const fileContent = `module.exports = {
-        config: ${JSON.stringify(safeConf, null, 4)},
-        paths: ${JSON.stringify(paths, null, 4)}
-      }`
+      const conf = JSON.stringify({ config: safeConf, paths: paths }, null, 2)
+      const fileContent = `module.exports = ${conf}
+`
       stream.once('open', function (fd) {
         stream.write(fileContent)
         stream.end()
