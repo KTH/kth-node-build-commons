845b3400d847d2b47674793c05d0f2c61b14f74a
jhsware <sebastian@urbantalk.se>
2016-12-20 13:53:05 +0100

    Using require with try/catch turns out to be simpler and more robust than checking file exists because we don’t have to worry about project path

--- buildConfig.js
+++ buildConfig.js
@@ -1,7 +1,6 @@
 'use strict'
 const configurator = require('kth-node-configuration')
 const path = require('path')
-const Promise = require('bluebird')
 let paths
 
 module.exports = {
@@ -20,48 +19,39 @@ module.exports = {
       process.env.NODE_ENV = env
 
       const tmpConfig = {
-        defaults: require(path.join(configBasePath, '/commonSettings')),
         local: require(path.join(configBasePath, '/localSettings'))
       }
 
-      const fs = Promise.promisifyAll(require('fs'))
-
-      // Optionally a dev-, ref- and prodSettings if the file exists
-      const promises = [
-        { 'name': 'ref', path: path.join(configBasePath, '/refSettings')},
-        { 'name': 'prod', path: path.join(configBasePath, '/prodSettings')},
-        { 'name': 'dev', path: path.join(configBasePath, '/devSettings')}].map(function (item) {
-        return fs.statAsync(item.path)
-          .then(function (stat) {
-            return Promise.resolve(item)
-          })
-          .catch(function () {
-            return Promise.resolve(undefined)
-          })
+      // Optionally add common-, dev-, ref- and prodSettings if the file exists
+      const optionalSettings = [
+        { 'name': 'default', path: path.join(configBasePath, '/commonSettings') },
+        { 'name': 'ref', path: path.join(configBasePath, '/refSettings') },
+        { 'name': 'prod', path: path.join(configBasePath, '/prodSettings') },
+        { 'name': 'dev', path: path.join(configBasePath, '/devSettings') }
+      ]
+      
+      optionalSettings.forEach((item) => {
+        try {
+          tmpConfig[item.name] = require(item.path)
+        } catch (err) {
+          // Do nothing with optional xxxSettings.js files that don't exist
+        }
       })
-      Promise.all(promises)
-        .then(function (results) {
-          results.forEach(function (result) {
-            if (result !== undefined) {
-              tmpConfig[result.name] = require(result.path)
-            }
-          })
-          return Promise.resolve(tmpConfig)
-        })
-        .then(function (tmpConfig) {
-          // Now we have added these settings and can generate the config-xxx.js file
-          const config = configurator(tmpConfig)
-          const safeConf = config.safe()
-          const stream = fs.createWriteStream(`public/js/app/config-${env}.js`)
 
-          const conf = JSON.stringify({ config: safeConf, paths: paths }, null, 2)
-          const fileContent = `module.exports = ${conf}
-    `
-          stream.once('open', function (fd) {
-            stream.write(fileContent)
-            stream.end()
-          })
-        })
+      // Now we have added these settings and can generate the config-xxx.js file
+      const config = configurator(tmpConfig)
+      const safeConf = config.safe()
+      
+      const fs = require('fs')
+      const stream = fs.createWriteStream(`public/js/app/config-${env}.js`)
+
+      const conf = JSON.stringify({ config: safeConf, paths: paths }, null, 2)
+      const fileContent = `module.exports = ${conf}
+`
+      stream.once('open', function (fd) {
+        stream.write(fileContent)
+        stream.end()
+      })
     })
   }
 }
