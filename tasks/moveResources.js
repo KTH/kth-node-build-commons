7d11d7c0471e15ab1a3879911ae3e941d3c029b6
jhsware <sebastian@urbantalk.se>
2017-01-10 13:32:19 +0100

    Removed browser-sync and attempted to return merged streams for proper async behaviour

--- tasks/moveResources.js
+++ tasks/moveResources.js
@@ -1,29 +1,38 @@
 const gulp = require('gulp')
 const del = require('del')
+const mergeStream = require('merge-stream')
 
 module.exports.moveKthStyle = function () {
-  return gulp.src(['./node_modules/kth-style/sass/**',
-            './node_modules/kth-style/img/**'])
+  return gulp.src([
+    './node_modules/kth-style/sass/**',
+    './node_modules/kth-style/img/**'
+  ])
     .pipe(gulp.dest('./public/css/kth-style/'))
 }
 
 module.exports.moveBootstrap = function () {
-  return gulp.src(['./node_modules/bootstrap/dist/css/bootstrap.min.css',
-            './node_modules/bootstrap/dist/css/bootstrap-theme.min.css'])
+  const bootstrap = gulp.src([
+    './node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+  ])
     .pipe(gulp.dest('./public/css/bootstrap/'))
-    .src('./node_modules/bootstrap/dist/fonts/**/*.{ttf,woff*,eof,svg}')
+
+  const fonts = gulp.src('./node_modules/bootstrap/dist/fonts/**/*.{ttf,woff*,eof,svg}')
     .pipe(gulp.dest('./public/css/fonts/'))
+
+  return mergeStream(bootstrap, fonts)
 }
 
 module.exports.moveFontAwesome = function () {
-  gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
+  const css = gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
     .pipe(gulp.dest('./public/css/font-awesome/'))
 
-  gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff*,eof,svg}')
+  const fonts = gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff*,eof,svg}')
     .pipe(gulp.dest('./public/css/fonts/'))
+
+  return mergeStream(css, fonts)
 }
 
 module.exports.cleanKthStyle = function () {
-  del(['./public/css/kth-style/*'])
-  del(['./public/img/kth-style/*'])
+  return del(['./public/css/kth-style/*', './public/img/kth-style/*'])
 }
