350104fb3f78f53e2e4acf38af5b764afb5258d7
jhsware <sebastian@urbantalk.se>
2017-01-10 12:43:44 +0100

    Always to sass transpile

--- tasks/sass.js
+++ tasks/sass.js
@@ -2,20 +2,14 @@ const gulpUtil = require('gulp-util')
 const gulp = require('gulp')
 const sass = require('gulp-sass')
 
-const { isProduction, isReference } = require('./common')
-
 module.exports = function () {
   gulpUtil.log(gulpUtil.colors.bold.underline.blue('Sass Transpile'))
 
-  // TODO: Use common to detect target env
-  if (isProduction() || isReference()) {
-    gulpUtil.log(gulpUtil.colors.green('Running Sass transpile'))
-    gulp.src(['./public/css/**/*.scss'])
-      .pipe(sass({
-        outputStyle: 'compressed'
-      }))
-      .pipe(gulp.dest('./public/css'))
-  } else {
-    gulpUtil.log(gulpUtil.colors.cyan('Did not run Sass transpile'))
-  }
+  gulpUtil.log(gulpUtil.colors.green('Running Sass transpile'))
+  gulp.src(['./public/css/**/*.scss'])
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
+    .pipe(gulp.dest('./public/css'))
+
 }
