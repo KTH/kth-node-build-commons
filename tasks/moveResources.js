fff8a7b739b349b4ce9afc7f79aa707080e320e3
jhsware <sebastian@urbantalk.se>
2017-01-11 15:14:31 +0100

    Compiling kth-style from source

--- tasks/moveResources.js
+++ tasks/moveResources.js
@@ -3,13 +3,8 @@ const del = require('del')
 const mergeStream = require('merge-stream')
 
 module.exports.moveKthStyle = function () {
-  const sass = gulp.src('./node_modules/kth-style/sass/**')
-    .pipe(gulp.dest('./public/css/kth-style/'))
-
-  const img = gulp.src('./node_modules/kth-style/img/**')
-    .pipe(gulp.dest('./public/img/kth-style/'))
-
-  return mergeStream(sass, img)
+  return gulp.src('./node_modules/kth-style/dist/img/**')
+    .pipe(gulp.dest('dist/img/kth-style'))
 }
 
 module.exports.moveBootstrap = function () {
@@ -17,20 +12,20 @@ module.exports.moveBootstrap = function () {
     './node_modules/bootstrap/dist/css/bootstrap.min.css',
     './node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
   ])
-    .pipe(gulp.dest('./public/css/bootstrap/'))
+    .pipe(gulp.dest('dist/css/bootstrap'))
 
   const fonts = gulp.src('./node_modules/bootstrap/dist/fonts/**/*.{ttf,woff*,eof,svg}')
-    .pipe(gulp.dest('./public/css/fonts/'))
+    .pipe(gulp.dest('dist/css/fonts'))
 
   return mergeStream(bootstrap, fonts)
 }
 
 module.exports.moveFontAwesome = function () {
   const css = gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('./public/css/font-awesome/'))
+    .pipe(gulp.dest('dist/css/font-awesome'))
 
   const fonts = gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff*,eof,svg}')
-    .pipe(gulp.dest('./public/css/fonts/'))
+    .pipe(gulp.dest('dist/css/fonts'))
 
   return mergeStream(css, fonts)
 }
@@ -38,5 +33,5 @@ module.exports.moveFontAwesome = function () {
 module.exports.cleanKthStyle = function () {
   // Performing this sync because we want to make sure nothing else is started
   // before this is completed
-  del.sync(['./public/css/kth-style/*', './public/img/kth-style/*'])
+  del.sync(['dist/css/kth-style/*', 'dist/img/kth-style/*'])
 }
